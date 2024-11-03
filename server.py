903cf8b82146600efc86079b19d0abf869f6742d
blakeplusplus <76140847+blakeplusplus@users.noreply.github.com>
2024-11-03 11:14:11 -0600

    Restyle maps.html, do more maps I think the new maps.html style looks better but will have to wait for feedback Finished even more maps (fuck this takes forever)

--- server.py
+++ server.py
@@ -20,6 +20,22 @@ def pass_arena2():
 def pass_stadium():
     return render_template('pass_stadium.html')
 
+@app.route("/pass_greenhouse")
+def pass_greenhouse():
+    return render_template('pass_greenhouse.html')
+
+@app.route("/pass_stonework")
+def pass_stonework():
+    return render_template('pass_stonework.html')
+
+@app.route("/pass_ufo")
+def pass_ufo():
+    return render_template('pass_ufo.html')
+
+@app.route("/pass_colosseum2")
+def pass_colosseum2():
+    return render_template('pass_colosseum2.html')
+
 if __name__ == "__main__":
     port = 5000
     if len(sys.argv) > 1:
@@ -28,4 +44,3 @@ if __name__ == "__main__":
         except ValueError:
             print("Invalid port number. Using default port 5000.")
     app.run(host='0.0.0.0', port=port)
-
